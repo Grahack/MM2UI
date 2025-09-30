b6a54bba963fb243686b81f5ca55770287826082
Christophe Gragnic <cgragnic@netc.fr>
2025-09-30 10:44:35 +0200

    Some other colours

--- MainComponent.cpp
+++ MainComponent.cpp
@@ -52,6 +52,10 @@ MainComponent::MainComponent()
                                       algosArray[j], j+1);
         }
         oscAlgosArray[i]->setSelectedId(1, juce::dontSendNotification);
+        oscAlgosArray[i]->setColour(juce::ComboBox::textColourId,
+                                    juce::Colours::black);
+        oscAlgosArray[i]->setColour(juce::ComboBox::backgroundColourId,
+                                    juce::Colour(0xffF76D22));
         oscAlgosArray[i]->addListener(this);
         addAndMakeVisible(*oscAlgosArray[i]);
     }
